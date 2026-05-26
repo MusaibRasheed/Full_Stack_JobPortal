d52abcb28f838e24c127b0ca4f447262bc6795c0
Musaib Rasheed <git config --global user.email 123456789+MusaibRasheed@users.noreply.github.com>
2026-05-27 00:09:20 +0530

    fix cors

--- jobportal-yt/backend/index.js
+++ jobportal-yt/backend/index.js
@@ -17,8 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
+    origin: [
+        'https://full-stack-job-portal-wtthfc261-musaibs-projects-5e2f747b.vercel.app',
+        'http://localhost:5173'
+    ],
+    credentials: true
 }
 
 app.use(cors(corsOptions));
