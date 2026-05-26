a29f7080082946cdcb179b275f57b6bba1069727
Musaib Rasheed <git config --global user.email 123456789+MusaibRasheed@users.noreply.github.com>
2026-05-26 17:33:36 +0530

    update footer with my name

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://full-stack-job-portal-okix-pzbgpe7sg-musaibs-projects-5e2f747b.vercel.app',
     credentials:true
 }
 
