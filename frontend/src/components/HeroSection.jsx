5b922025c21fce80dbeb13340adf3e008782ea5d
Musaib Rasheed <git config --global user.email 123456789+MusaibRasheed@users.noreply.github.com>
2026-05-26 16:55:00 +0530

    update footer with my name

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -18,7 +18,7 @@ const HeroSection = () => {
     return (
         <div className='text-center'>
             <div className='flex flex-col gap-5 my-10'>
-                <span className=' mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium'>No. 1 Job Hunt Website</span>
+                <span className=' mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium'>Find Your Dream Job Today</span>
                 <h1 className='text-5xl font-bold'>Search, Apply & <br /> Get Your <span className='text-[#6A38C2]'>Dream Jobs</span></h1>
                 <p>Find your dream job with ease. Connect with top companies and apply for the best opportunities in your field.</p>
                 <div className='flex w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto'>
